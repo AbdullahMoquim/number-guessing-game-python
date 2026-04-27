89e76acf55bbe9ca7c65e226a51a1e2e9e42e20c
AbdullahMoquim <abodi.mokeem@gmail.com>
2026-04-26 21:02:23 -0400

    Add welcome screen and game features
    Added a welcome message and the game features for the user.

---
+++ number_guessing_game.py
@@ -0,0 +1,14 @@
+import random
+
+def main()
+  print("""
+  Welcome to the Number Guessing Game!
+      Game Features:
+      1. Choose Easy, Medium, or Hard difficulty.
+      2. Each level has a different number range and number of attempts.
+      3. Get Too High / Too Low hints.
+      4. Attempts are tracked.
+      5. Performance shown after each round.
+      6. Replay after the game ends.
+  """)
+  
